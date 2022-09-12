f33c700c59761421cb6545f4837fd827d09325c8
Soufiyan <ahminsoufiyan@gmail.com>
2022-09-12 16:27:04 +0100

    Linking client side to server side with socket io

--- src/Server/socketConnection.js
+++ src/Server/socketConnection.js
@@ -1,5 +1,8 @@
-const io = require("socket.io")(3003);
-
+const io = require("socket.io")(3003, {
+  cors: {
+    origin: ["http://localhost:3000"],
+  },
+});
 io.on("connection", (socket) => {
   console.log(socket.id);
 });
