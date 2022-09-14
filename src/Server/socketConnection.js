a429b728a6eeefb99d46bdadb3a19424b1ec53ff
Soufiyan <ahminsoufiyan@gmail.com>
2022-09-14 01:57:23 +0100

    Moved Checking user entries to server side

--- src/Server/socketConnection.js
+++ src/Server/socketConnection.js
@@ -1,3 +1,58 @@
+//////////////
+
+const { MongoClient } = require("mongodb");
+
+// Connection URL
+const url = "mongodb://0.0.0.0:27017";
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = "wordleProject";
+let word = "";
+let colorsArr = [];
+async function main(action, socketID, word, tryWord) {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log("Connected successfully to server");
+  const db = client.db(dbName);
+  const collection = db.collection("liveGames");
+
+  // the following code inject into the collection liveGames
+  if (action === "init") {
+    const insertResult = await collection.insertMany([
+      { player_token: socketID, random_word: word },
+    ]);
+  }
+  if (action === "find") {
+    const filteredDocs = await collection
+      .find({ player_token: socketID })
+      .toArray();
+    let newData = filteredDocs[0].random_word;
+    console.log(newData);
+
+    colorsArr = [];
+
+    tryWord.forEach((c, key) => {
+      const realC = newData[key];
+      if (realC === c) {
+        colorsArr.push("#0f0");
+      } else {
+        const isExist = newData.split("").some((el) => el === c);
+        if (isExist) {
+          colorsArr.push("#ff0");
+        } else {
+          colorsArr.push("#d4d5ce");
+        }
+      }
+    });
+  }
+
+  // done
+  return "done.";
+}
+
+//////////////////////////////
+
 const dataWords = [
   "WINER",
   "BEFOR",
@@ -10,8 +65,6 @@ const dataWords = [
   "FAKER",
   "LOSER",
 ];
-let word = "";
-let colorsArr = [];
 
 const io = require("socket.io")(3003, {
   cors: {
@@ -21,14 +74,20 @@ const io = require("socket.io")(3003, {
 io.on("connection", (socket) => {
   console.log("SOCKET CONNECTING " + socket.id);
   setRandomWordle();
+  main("init", socket.id, word)
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());
 
   socket.on("disconnect", function () {
     console.log("SOCKET DISCONNECTING " + socket.id);
   });
   socket.on("submitted-word", (tryWord) => {
     console.log(tryWord);
-    checkWord(tryWord);
-    io.to(socket.id).emit("treated-colors", colorsArr);
+    main("find", socket.id, word, tryWord)
+      .then(() => io.to(socket.id).emit("treated-colors", colorsArr))
+      .catch(console.error)
+      .finally(() => client.close());
   });
 });
 
@@ -40,6 +99,7 @@ function setRandomWordle() {
 
 function checkWord(tryWord) {
   colorsArr = [];
+
   tryWord.forEach((c, key) => {
     const realC = word[key];
     if (realC === c) {
